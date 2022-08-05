ce6de2e4ed686ff87fde611f12b0d098108e2cd8
zdu <zdu.strong@gmail.com>
2022-08-05 19:45:06 +0800

    #: Upgrade version

--- lib/lib/useMobxState.tsx
+++ lib/lib/useMobxState.tsx
@@ -1,25 +1,25 @@
-import { extendObservable, remove, isObservable, keys } from 'mobx';
+import { extendObservable, remove, isObservable, runInAction } from 'mobx';
 import { useLocalObservable } from 'mobx-react-lite';
 
 export function useMobxState<T extends Record<any, any>, P extends Record<any, any>>(state: T | (() => T), props?: P | (() => P)): T & P {
     const mobxState = useLocalObservable(typeof state === "function" ? (state as any) : () => state);
 
-    const propsResult = typeof props === "function" ? (props as any)() : props;
+    runInAction(() => {
+        const propsResult = typeof props === "function" ? (props as any)() : props;
 
-    if (propsResult) {
-        for (const key in propsResult) {
-            if (isObservable(propsResult[key])) {
-                mobxState[key] = propsResult[key];
-            } else {
-                if (propsResult[key] !== mobxState[key]) {
-                    if(keys(mobxState).includes(key)){
+        if (propsResult) {
+            for (const key in propsResult) {
+                if (isObservable(propsResult[key])) {
+                    mobxState[key] = propsResult[key];
+                } else {
+                    if (propsResult[key] !== mobxState[key]) {
                         remove(mobxState, key);
+                        extendObservable(mobxState, { [key]: propsResult[key] }, { [key]: false })
                     }
-                    extendObservable(mobxState, { [key]: propsResult[key] }, { [key]: false })
                 }
             }
         }
-    }
+    })
 
     return mobxState as any;
 }
