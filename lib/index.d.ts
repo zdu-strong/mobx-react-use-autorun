a5d29e180d3a92682761d9dfe9f25f272e82450b
zdu <zdu.strong@gmail.com>
2022-08-05 18:52:50 +0800

    #: Upgrade version

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare function useMobxState<T extends Record<any, any>, P extends Record<any, any>>(state: T, props?: P): T & P;
-declare function useMobxState<T extends Record<any, any>, P extends Record<any, any>>(statefn: () => T, propsfn: () => P): T & P;
-declare function useMobxState<T extends Record<any, any>, P extends Record<any, any>>(statefn: () => T, props?: P): T & P;
-declare function useMobxState<T extends Record<any, any>, P extends Record<any, any>>(state: T, propsfn: () => P): T & P;
-declare function useMobxState<T extends Record<any, any>>(state: T): T;
-declare function useMobxState<T extends Record<any, any>>(statefn: () => T): T;
-export { useMobxState };
--- lib/lib/useMobxState.tsx
+++ lib/lib/useMobxState.tsx
@@ -1,13 +1,20 @@
 import { extendObservable, remove, isObservable } from 'mobx';
 import { useLocalObservable } from 'mobx-react-lite';
 
-export function useMobxState<T extends Record<any, any>, P extends Record<any, any>>(state: T, props?: P): T & P {
+export function useMobxState<T extends Record<any, any>, P extends Record<any, any>>(state: T, props: P): T & P;
+export function useMobxState<T extends Record<any, any>, P extends Record<any, any>>(statefn: () => T, propsfn: () => P): T & P;
+export function useMobxState<T extends Record<any, any>, P extends Record<any, any>>(state: T, propsfn: () => P): T & P;
+export function useMobxState<T extends Record<any, any>, P extends Record<any, any>>(statefn: () => T, props: P): T & P;
+export function useMobxState<T extends Record<any, any>>(state: T): T;
+export function useMobxState<T extends Record<any, any>>(statefn: () => T): T;
+
+export function useMobxState(state: any, props?: any): any {
 
     const mobxState = useLocalObservable(typeof state === "function" ? (state as any) : () => state);
 
     const propsResult = typeof props === "function" ? props() : props;
 
-    if(propsResult){
+    if (propsResult) {
         for (const key in propsResult) {
             if (isObservable(propsResult[key])) {
                 mobxState[key] = propsResult[key];
