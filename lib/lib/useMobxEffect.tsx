ee3d7936e80d14584aa6daa9a181f4d1c2987444
zdu <zdu.strong@gmail.com>
2022-08-07 19:51:08 +0800

    #: Upgrade version

--- lib/lib/useMobxEffect.tsx
+++ lib/lib/useMobxEffect.tsx
@@ -2,21 +2,30 @@ import { useMount, useUnmount } from 'react-use';
 import { Subscription } from 'rxjs';
 import { reaction, toJS } from 'mobx'
 import { useMobxState } from './useMobxState';
+import { useEffect } from 'react';
 
-export const useMobxEffect = (callback: () => void, dependencyList: any[]): void => {
+export const useMobxEffect = (callback: () => void, dependencyList?: any[]): void => {
 
     const state = useMobxState({
         subscription: new Subscription(),
     }, Object.assign({}, dependencyList))
 
-    const source = useMobxState({},{callback});
+    const source = useMobxState({}, { callback });
+
+    useEffect(() => {
+        if (!dependencyList) {
+            callback()
+        }
+    })
 
     useMount(() => {
-        const disposer = reaction(() => [toJS(state)], () => source.callback(), { fireImmediately: true, delay: 1 });
+        if (dependencyList) {
+            const disposer = reaction(() => [toJS(state)], () => source.callback(), { fireImmediately: true, delay: 1 });
 
-        state.subscription.add(new Subscription(() => {
-            disposer();
-        }));
+            state.subscription.add(new Subscription(() => {
+                disposer();
+            }));
+        }
     })
 
     useUnmount(() => {
