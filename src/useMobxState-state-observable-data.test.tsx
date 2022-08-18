7dbe5043ecefb4129077f75512117a3bc110877c
zdu <zdu.strong@gmail.com>
2022-08-18 20:31:27 +0800

    #: Upgrade version

---
+++ src/useMobxState-state-observable-data.test.tsx
@@ -0,0 +1,23 @@
+import { render } from '@testing-library/react'
+import { useMobxState } from '../lib';
+import { isObservable, observable } from 'mobx'
+
+test('', () => {
+    const result = render(<TestComponent data={initData} />);
+    expect(isObservable(lastData)).toBeTruthy()
+})
+
+function TestComponent(props: { data: { people: { name: string } } }) {
+    const state = useMobxState(() => ({ people: { name: "tom" } }), {
+        ...props
+    });
+
+    lastData = state.data;
+
+    return <>
+        {state.people.name}
+    </>;
+}
+
+const initData = observable({ people: { name: "tom" } })
+let lastData = { } as any;
