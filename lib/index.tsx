c2c87a979968061798df3c72865b0a7414bbefc3
zdu <zdu.strong@gmail.com>
2023-03-27 10:10:02 +0800

    #: Upgrade version to 3.1.15
    export makeAutoObservable from mobx package

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,10 +1,10 @@
 import './js/mobx_config'
-import { toJS, observable } from 'mobx';
+import { toJS, observable, makeAutoObservable } from 'mobx';
 import { observer } from 'mobx-react-lite';
 import { useMobxEffect } from './js/useMobxEffect';
 import { useMobxState } from './js/useMobxState';
 import { useMount, useUnmount } from 'react-use';
 
-export { toJS, observable, observer }
+export { toJS, observable, observer, makeAutoObservable }
 export { useMobxState, useMobxEffect }
 export { useMount, useUnmount }
