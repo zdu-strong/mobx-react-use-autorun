cc023e797c4a1fe7d472ed2d1bb371a9a3a0574f
John Williams <zdu.strong@gmail.com>
2025-10-09 06:18:53 +0900

    feat: Upgrade version to 4.0.66-beta
    Refactor the useMobxEffect method

--- lib/js/useMobxEffect.tsx
+++ lib/js/useMobxEffect.tsx
@@ -1,20 +1,20 @@
 import { useMount } from './useMount';
-import { ReplaySubject, Subscription, tap } from 'rxjs';
-import { makeAutoObservable, reaction, toJS } from 'mobx'
-import { useEffect, useRef } from 'react';
-import { extendObservable, runInAction } from 'mobx';
+import { reaction, toJS } from 'mobx'
+import { useEffect } from 'react';
+import { useMobxState } from './useMobxState';
 
 export const useMobxEffect = (callback: () => void, dependencyList?: any[]): void => {
 
-    const callbackRef = useRef<{ callback: () => void }>({ callback: callback });
+    const state = useMobxState({
+    }, {
+        callback
+    });
 
-    const subjectRef = useRef(new ReplaySubject<void>(1));
+    const dependencyListState = useMobxState({
 
-    const dependencyListRef = useRef(dependencyList);
-
-    callbackRef.current!.callback = callback;
-
-    dependencyListRef.current = dependencyList;
+    }, {
+        ...(dependencyList ? dependencyList : {})
+    })
 
     useEffect(() => {
         if (dependencyList) {
@@ -23,35 +23,14 @@ export const useMobxEffect = (callback: () => void, dependencyList?: any[]): voi
         callback();
     })
 
-    useEffect(() => {
-        if (!dependencyList) {
-            return;
-        }
-        subjectRef.current.next();
-    }, dependencyList)
-
     useMount((subscription) => {
         if (!dependencyList) {
             return;
         }
-        const mobxData = makeAutoObservable(Object.assign({}, dependencyListRef.current));
-        subscription.add(subjectRef.current.pipe(
-            tap(() => {
-                runInAction(() => {
-                    const props = Object.assign({}, dependencyListRef.current);
-                    for (const key in props) {
-                        extendObservable(mobxData, { [key]: props[key] }, { [key]: false });
-                    }
-                })
-            })
-        ).subscribe());
 
-        const disposer = reaction(() => [toJS(mobxData)], () => callbackRef.current!.callback(), { fireImmediately: true, delay: 1 });
+        const disposer = reaction(() => [toJS(dependencyListState)], () => state.callback(), { fireImmediately: true, delay: 1 });
 
-        subscription.add(new Subscription(() => {
-            disposer();
-        }));
-
+        subscription.add(() => disposer());
     })
 
 }
