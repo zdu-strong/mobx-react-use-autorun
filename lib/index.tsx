80e456663b630d3d80a6f5c2761f5757d3c184ef
John Williams <zdu.strong@gmail.com>
2026-02-24 16:43:11 +0900

    feat: Upgrade version to 4.0.71-beta
    Update versions of dependencies

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,10 +1,10 @@
 import './js/mobx_config'
-import { toJS, observable, makeAutoObservable, makeObservable } from 'mobx';
+import { toJS, observable, makeAutoObservable } from 'mobx';
 import { observer } from 'mobx-react';
 import { useMobxEffect } from './js/useMobxEffect';
 import { useMobxState } from './js/useMobxState';
 import { useMount } from './js/useMount';
 
-export { toJS, observable, observer, makeAutoObservable, makeObservable }
+export { toJS, observable, observer, makeAutoObservable }
 export { useMobxState, useMobxEffect }
 export { useMount }
